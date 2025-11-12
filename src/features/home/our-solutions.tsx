2614218a8f0100f7d67ee94d546cf2295079f2f8
Naimul <naimulislam920@gmail.com>
2025-11-13 02:02:29 +0600

    Add collaborators carousel component is home page

---
+++ src/features/home/our-solutions.tsx
@@ -0,0 +1,14 @@
+export default function OurSolutions() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eaque
+      aliquid commodi maiores placeat illum iure molestiae nemo et eligendi
+      officia perferendis doloribus cum nostrum voluptate reprehenderit ipsam,
+      excepturi sed incidunt itaque ut nobis natus quod minima! Non enim
+      accusamus possimus incidunt aut reprehenderit, dicta omnis veniam nam
+      eaque quibusdam velit eos architecto dolorem voluptatum at accusantium
+      assumenda suscipit adipisci maxime nostrum! Ex ab, iure natus eveniet
+      recusandae quod atque.
+    </div>
+  );
+}
